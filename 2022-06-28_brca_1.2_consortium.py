1a76417b16b2b7a9329cf7ee3e0cb3d3f4b2d05f
thomasyu888 <thomas.yu@sagebase.org>
2022-06-28 06:57:53 -0700

    Update release version

--- 2022-06-28_brca_1.2_consortium.py
+++ 2022-06-28_brca_1.2_consortium.py
@@ -8,14 +8,21 @@ restrictions and install from source.
     synapse cp syn24981909 --destinationId syn32298950 --setProvenance None
 
 2. Update release_version column in the clinical files
+3. Update meta_study.txt
 """
 import pandas as pd
 import synapseclient
 
 syn = synapseclient.login()
 
+git_url = "https://github.com/Sage-Bionetworks/genie-project-requests/blob/main/2022-06-28_brca_1.2_consortium.py"
+
 # Update release_version
 release_12_files = syn.getChildren("syn32299038")
 for each_file in release_12_files:
     ent = syn.get(each_file['id'])
-    ent_df = pd.read_csv()
+    ent_df = pd.read_csv(ent.path)
+    ent_df['release_version'] = "1.2"
+    ent_df.to_csv(ent.name, index=False)
+    ent.path = ent.name
+    syn.store(ent, executed=git_url)

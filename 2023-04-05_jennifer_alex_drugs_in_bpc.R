1f01a126a1613d39d7151d8af4afdc9ac9a3007c
alexpaynter <alexcpaynter@gmail.com>
2023-05-09 22:50:02 +0000

    removed comparison with prev output

--- 2023-04-05_jennifer_alex_drugs_in_bpc.R
+++ 2023-04-05_jennifer_alex_drugs_in_bpc.R
@@ -188,42 +188,6 @@ dft_cohort_comb <- left_join(
 
 
 
-
-###############################
-# Checking with previous work #
-###############################
-
-ent <- synGet(prev_xlsx_work_synid)
-jen_xl <- readxl::read_xlsx(ent$path,
-                            sheet = 1,
-                            col_names = "chr") 
-# The excel sheet is a pivot table, so we have the cohort names smashed in
-#   with the drugs that were discovered.  Let's remove the cohort labels 
-#   and a few other strings that won't help us:
-cohort_names <- c("Breast", "Prostate", "NSCLC", "Bladder", "CRC", "PANC",
-                  "(blank)", "Grand Total", "BPC Agents with Corresponding Cohort")
-ind_drugs_found_xl <- jen_xl %>%
-  filter(!(chr %in% cohort_names)) %>%
-  pull(chr)
-
-ind_drugs_found_r <- dft_cohort_comb %>%
-  filter(obs_index %in% 1) %>%
-  pull(drug_name) %>%
-  # remove the parenthetical info from the drugs to match Jen's style:
-  str_replace(., pattern = "\\(.*", "") %>%
-  str_trim() %>%
-  unique
-
-# What are the differences?
-setdiff(ind_drugs_found_r,
-        ind_drugs_found_xl) %>%
-  paste(collapse = ", ")
-setdiff(ind_drugs_found_xl,
-        ind_drugs_found_r)
-# Good!  No drugs found in excel that weren't found in R.  Just a handful found
-#   in R but not excel.
-
-
 # Collapsing the tidier format into something better for the end user:
 dft_cohort_comb %<>%
   mutate(drug_name = str_replace(drug_name, pattern = "\\(.*", ""),

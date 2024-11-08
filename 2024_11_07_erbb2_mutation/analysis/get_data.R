1eb458b94bf8a4f0d372496eaca8f1d9657bfb39
alexpaynter <alexcpaynter@gmail.com>
2024-11-07 21:17:50 -0800

    new request

---
+++ 2024_11_07_erbb2_mutation/analysis/get_data.R
@@ -0,0 +1,30 @@
+library(synapser)
+library(here)
+
+synLogin()
+
+get_synapse_entity_data_in_tsv <- function(synapse_id, 
+                                           version = NA) {
+    if (is.na(version)) {
+        entity <- synGet(synapse_id)
+    } else {
+        entity <- synGet(synapse_id, version = version)
+    }
+    
+    data <- readr::read_tsv(entity$path,
+                            show_col_types = F,
+                            progress = F)
+    return(data)
+}
+
+mut <- get_synapse_entity_data_in_tsv(
+    synapse_id = 'syn5571527'
+)
+
+clin <- get_synapse_entity_data_in_tsv(
+    synapse_id = 'syn7392892'
+)
+
+readr::write_rds(mut, here('data', 'mut.rds'))
+readr::write_rds(clin, here('data', 'clin.rds'))
+    
---
+++ 2024_11_07_erbb2_mutation/analysis/process.R
@@ -0,0 +1,147 @@
+library(here)
+library(readr)
+library(tidyverse)
+library(magrittr)
+
+mut <- readr::read_rds(
+    here('data', 'mut.rds')
+)
+
+clin <- readr::read_rds(
+    here('data', 'clin.rds')
+)
+
+mut %<>% rename_all(tolower)
+clin %<>% rename_all(tolower)
+
+vec_sponsor_list <- c(
+    "R103Q", "I767F", "V777M",
+    "D277Y", "I767M", "V777A",
+    "G309A", "D769Y", "P780_Y781insGSP/G778_P780dup",
+    "G309E", "D769N", "V777_G778insCG",
+    "S310P", "D769H", "V777_G778insG",
+    "S310F", "A775_G776insYVMA/Y772_A775dup", "V777_G778insV",
+    "S310Y", "A775_G776insV", "G778_S779insLPG",
+    "E321G", "A775_G776insTVMA", "G778_S779insCPG",
+    "R330Q", "G776delinsLC", "G778_S779insLPS",
+    "S335C", "G776C", "G778_S779insG",
+    "S653C", "G776S", "S779_P780insVGS",
+    "V659E", "G776delinsCV", "T798I",
+    "G660D", "G776delinsVC", "T798M",
+    "R678Q", "G776V", "V842I",
+    "T733I", "G776_V777insL", "V842E",
+    "L755A", "G776_V777insVGS", "T862I",
+    "L755F", "G776_V777insVGC", "R868W",
+    "L755M", "G776_V777insVC", "L869R",
+    "L755P", "V777delinsCVCG", "R896C",
+    "L755S", "V777L", "R896H",
+    "L755W"
+)
+
+# obtained from the website.  mskcc.oncotree pacakge doesn't seem to have the
+#.  right structure, this was easier.
+vec_nsclc_oncotree <- c(
+    'NSCLC', 'CMPT', 
+    'LCLC', 'BLCLC', 'CCLC', 'GCLC', 'RLCLC', 'LECLC',
+    'LUAD', 'LUAS', 'LUSC', 'NUTCL', 'LUPC', 'NSCLCPD',
+    'SGTTL', 'LUACC', 'LUMEC',
+    'SPCC'
+)
+
+clin_nsclc <- clin %>% 
+    filter(
+        oncotree_code %in% vec_nsclc_oncotree
+    ) %>%
+    select(sample_id, oncotree_code, patient_id, seq_year)
+
+nsclc_erbb2 <- mut %>%
+    filter(hugo_symbol %in% "ERBB2") %>%
+    inner_join(
+        .,
+        clin_nsclc,
+        by = c(tumor_sample_barcode = "sample_id")
+    ) 
+
+
+vec_sponsor_list <- c(
+    "R103Q", "I767F", "V777M",
+    "D277Y", "I767M", "V777A",
+    # Split these up:
+    "G309A", "D769Y", "P780_Y781insGSP",
+    "G309E", "D769N", "V777_G778insCG",
+    "S310P", "D769H", "V777_G778insG",
+    "S310F", "A775_G776insYVMA", "V777_G778insV",
+    "S310Y", "A775_G776insV", "G778_S779insLPG",
+    "E321G", "A775_G776insTVMA", "G778_S779insCPG",
+    "R330Q", "G776delinsLC", "G778_S779insLPS",
+    "S335C", "G776C", "G778_S779insG",
+    "S653C", "G776S", "S779_P780insVGS",
+    "V659E", "G776delinsCV", "T798I",
+    "G660D", "G776delinsVC", "T798M",
+    "R678Q", "G776V", "V842I",
+    "T733I", "G776_V777insL", "V842E",
+    "L755A", "G776_V777insVGS", "T862I",
+    "L755F", "G776_V777insVGC", "R868W",
+    "L755M", "G776_V777insVC", "L869R",
+    "L755P", "V777delinsCVCG", "R896C",
+    "L755S", "V777L", "R896H",
+    "L755W"
+) 
+
+# decided to resort these, don't mind this mess too much.
+vec_sponsor_list <- c(
+    vec_sponsor_list[seq.int(1, length(vec_sponsor_list), 3)],
+    vec_sponsor_list[seq.int(2, length(vec_sponsor_list), 3)],
+    vec_sponsor_list[seq.int(3, length(vec_sponsor_list), 3)]
+)
+
+nsclc_erbb2 %<>% mutate(
+    hgvsp_short_trim = str_replace(hgvsp_short, "^p.", ""),
+    hgvsp_short_trim = case_when(
+        # arbitrarily mapping to the first one in their list:
+        hgvsp_short_trim %in% "G778_P780dup" ~ "P780_Y781insGSP",
+        hgvsp_short_trim %in% "Y772_A775dup" ~ "A775_G776insTVMA",
+        T ~ hgvsp_short_trim
+    ),
+    act_list = hgvsp_short_trim %in% vec_sponsor_list
+)
+
+# Three numbers in Jumi's slide:
+n_nsclc <- clin_nsclc %>% count(patient_id) %>% nrow
+n_nsclc_erbb2 <- nsclc_erbb2 %>% count(patient_id) %>% nrow # number with Erbb2
+nsclc_erbb2 %>% filter(act_list) %>% count(patient_id) %>% nrow # with erbb2 act mut.
+
+dfp_mut_counts_in_list <- nsclc_erbb2 %>%
+    filter(act_list) %>%
+    # get one row per patient/hgvsp code - just in case of dupes.
+    count(hgvsp_short_trim, patient_id) %>% 
+    count(hgvsp_short_trim, name = 'people') %>%
+    mutate(
+        hgvsp_short_trim = factor(hgvsp_short_trim, levels = vec_sponsor_list)
+    ) %>%
+    complete(hgvsp_short_trim, fill = list(people = 0)) %>%
+    mutate(
+        prop_nsclc = people / n_nsclc,
+        prop_nsclc_erbb2 = people / n_nsclc_erbb2
+    ) %>%
+    arrange(hgvsp_short_trim)
+
+dfp_all_erbb2_muts <- nsclc_erbb2 %>%
+    count(hgvsp_short_trim, act_list, name = "alterations") %>%
+    arrange(desc(alterations)) 
+
+readr::write_rds(
+    dfp_mut_counts_in_list,
+    here('data', 'mut_counts_in_list.rds')
+)
+
+readr::write_rds(
+    dfp_all_erbb2_muts,
+    here('data', 'all_erbb2.rds')
+)
+
+readr::write_rds(
+    c(n_nsclc, n_nsclc_erbb2, 
+      (nsclc_erbb2 %>% filter(act_list) %>% count(patient_id) %>% nrow)),
+    here('data', 'three_numbers.rds')
+)

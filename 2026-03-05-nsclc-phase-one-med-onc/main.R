9786a3920d51568f1963e904d1e1ecb7612c1e8f
alexpaynter <alexcpaynter@gmail.com>
2026-03-06 11:33:05 -0800

    Added a main file

---
+++ 2026-03-05-nsclc-phase-one-med-onc/main.R
@@ -0,0 +1,6 @@
+library(here)
+source(here('analysis', 'script', 'get_raw_data.R'))
+source(here('analysis', 'script', 'subset_med_onc.R'))
+
+# And if you want to see where the visit number linkage fails open this file and run interactively:
+source(here('analysis', 'script', 'med_onc_number_linkage_failure.R'))
